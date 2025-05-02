a585c868c4237bb23eb083a0fdcc9d7377446fe3
muhammadjon8 <90293805+muhammadjon8@users.noreply.github.com>
2025-05-02 17:47:57 +0500

    s

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import router from "./routes";
+import { sendDistractedDrivingAlert } from "./controllers/bot.controller";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.use(router);
+app.post("/distracted", sendDistractedDrivingAlert);
+// app.use(router);
 app.get("/", (req, res) => {
   res.send(
     '<h1 style="color:green; text-align:center; margin-top: 300px">Welcome!<br> The Url is working</h1>'
